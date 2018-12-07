328972f8fe091a6b29f2d35ee48c7bd59489a5da
Ivan <meyakovenkoj@yandex.ru>
2018-12-07 13:48:35 +0300

    Initial Commit

---
+++ myarray/myarray.hpp
@@ -0,0 +1,22 @@
+//
+//  myarray.hpp
+//  myarray
+//
+//  Created by Ivan Yakovenko on 07/12/2018.
+//  Copyright © 2018 Ivan Yakovenko. All rights reserved.
+//
+
+#ifndef myarray_
+#define myarray_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class myarray
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ myarray/myarrayPriv.hpp
@@ -0,0 +1,18 @@
+//
+//  myarrayPriv.hpp
+//  myarray
+//
+//  Created by Ivan Yakovenko on 07/12/2018.
+//  Copyright © 2018 Ivan Yakovenko. All rights reserved.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class myarrayPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
